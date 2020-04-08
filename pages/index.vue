0192f8e19ea1cc1cb4117e411418f9de8fc36d20
Jik Torres <jikiboi03@gmail.com>
2020-04-08 15:14:35 +0800

    [feat] loading

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,12 @@
   <div class="container">
     <div class="notification">
       <h2>Fineract NUXT Member Module</h2>
+
+      <b-notification :closable="false">
+        <b-loading :is-full-page="isFullPage" :active.sync="isLoading">
+          <b-icon pack="fas" icon="sync-alt" size="is-large" custom-class="fa-spin"></b-icon>
+        </b-loading>
+      </b-notification>
     </div>
   </div>
 </template>
@@ -9,6 +15,12 @@
 <script>
 import https from 'https'
 export default {
+  data() {
+    return {
+      isLoading: false,
+      isFullPage: true
+    }
+  },
   head() {
     return {
       title: 'Member Module',
@@ -21,19 +33,29 @@ export default {
       ]
     }
   },
+  methods: {
+    openLoading() {
+      this.isLoading = true
+    },
+    stopLoading() {
+      this.isLoading = false
+    }
+  },
   mounted() {
     const data = {
-      username: "mifos",
-      password: "password"
+      username: 'mifos',
+      password: 'password'
     }
+    this.openLoading()
     this.$store
-      .dispatch("login", data)
+      .dispatch('login', data)
       .then(() => {
-        console.log("Logged in!")
+        this.stopLoading()
+        console.log('Logged in!')
       })
       .catch(err => {
         console.log(err)
-      });
+      })
   }
 }
 </script>
